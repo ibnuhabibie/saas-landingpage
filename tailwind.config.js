d2841d70d4bcb8d44e7537297695128156fbd5e5
Ibnu Habibie <bhentz94@gmail.com>
2023-02-16 21:30:00 +0700

    add header and hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,25 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 module.exports = {
-    content: ['./*.html'],
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-        extend: {
-            colors: {
-                //add your own color
-                //https://tailwindcss.com/docs/customizing-colors
-            },
-            container: {
-                center: true,
-            },
-        },
+  content: ["./*.html"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "dark-blue": "#060B27",
+        accent: "#7214FF",
+        //add your own color
+        //https://tailwindcss.com/docs/customizing-colors
+      },
+      fontFamily: {
+        satoshi: ["Satoshi", "cursive"],
+      },
+      container: {
+        center: true,
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
-}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
